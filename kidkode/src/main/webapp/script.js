3a030ec32a3a1c5c63298c9e2b6970f35f397adc
Tengku Hana Shafikah <tghanashafikah@gmail.com>
2020-07-08 15:02:02 -0400

    Edit script to have page display quiz questions and points gained for each career.

--- kidkode/src/main/webapp/script.js
+++ kidkode/src/main/webapp/script.js
@@ -1,48 +1,151 @@
-const artQuestions = [
-  {
-    number: 0, 
-    question: "Do you like playing video games?",
-    answers: {
-      /*store current points in a hash map so its easy to iterate over
-      the points array below and add a point to that entry in the map */
-      yes: {points: ["gameDev","animation", "2-dAnimation"], next:1 },
-      no:  {points: ["intDesign","webDesign", "UX"], next:2 },
-    },
-    previous: null //since it is the first question there is no previous   
-  },
-    { 
-        number: 1, 
-        question: "Do you like making art on the computer?",
-        answers: {
-        yes: {points: ["gameArt","animation", "3-dAnimation"], next:3 },
-        no:  {points: ["gameDev","conceptArt", "2-dAnimation"], next:4 },
-        },
-        previous: 0  //previous method will have to subtract point that was just added to hashmap
-       
-    },
-];
-
 /*this function is useful that when we update the map we can 
 just call getorDefault to get the value (or 0 if the key wasn't in the map) 
 without having to worry if that key was already there or not (see example below)*/
 
 function getOrDefault(key, map) {
-    let res = map.get(key); 
-    if (res) {
-        return res;
-    }
-    else {
-        return 0;
-    }
+  let res = map.get(key);
+  if (res) {
+    return res;
+  } else {
+    return 0;
+  }
 }
-/*example here for how updating the points in a hashmap would work.
-/ We would iterate through the points array in the current question's 
-object and update/add each career and point value to the hashmap*/
-$( document ).ready(function() {
-   let resultTracker = new Map();
-   let key = "gameArt";
-   result.set(key, getOrDefault(key, resultTracker) + 1);
-   console.log(getOrDefault(key, resultTracker));
-});
 
-  
+
+/*
+ * BUILD QUIZ
+ * This function takes each question and their options & builds the HTML
+ * elements of the quiz.
+ */
+function buildQuiz() {
+  // variable to store the HTML output
+  const output = [];
+
+  // for each question...
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    // variable to store the list of possible answers
+    const answers = [];
+
+    // and for each available answer...
+    for (var option in currentQuestion.answers) {
+      // ...add an HTML radio button
+      answers.push(
+        `<label>
+              <input type="radio" name="question${questionNumber}" value="${option}">
+              ${currentQuestion.answers[option].choice}
+          </label>`
+      );
+    }
+
+    // add this question and its answers to the output
+    output.push(
+      `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")} </div>`
+    );
+  });
+
+  // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join("");
+}
+
+
+/*
+ * SHOW RESULTS
+ * First, we check whether the user picked "yes" or "no" as their choice for each
+ * question. Based on their choice, we add the careers under that choice into a 
+ * hashmap, and increment the points for the corresponding careers in the hashmap
+ */
+function showResults() {
+  // get answer containers from our quiz
+  const answerContainers = quizContainer.querySelectorAll(".answers");
+
+  // create empty hashmap for the user's results
+  let resultTracker = new Map();
+
+  // for each question interate through each carrer in points array...
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    for (var i in currentQuestion.answers) {
+      if (userAnswer === currentQuestion.answers[i].choice) {
+        // ...and increment points for appropriate careers in hashmap
+        let pointsArr = currentQuestion.answers[i].points;
+        for (var j in pointsArr) {
+          let key = pointsArr[j];
+          resultTracker.set(key, getOrDefault(key, resultTracker) + 1);
+        }
+      }
+    }
+  });
+
+  // create a variable to store the HTML output
+  const output = [];
+
+  // add results from hashmap into output
+  function createHTMLElements(value, key, resultTracker) {
+    output.push(
+      `<h2> ${key} </h2>
+        <h3> ${value} </h3>`
+    );
+  }
+  resultTracker.forEach(createHTMLElements);
+
+  // show total points for each category in hashmap
+  resultsContainer.innerHTML = output.join("");
+}
+
+
+/*
+ * VARIABLES
+ */
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
+const myQuestions = [
+  {
+    number: 0,
+    question: "Do you like to play videogames?",
+    answers: {
+      yes: {
+        choice: "yes",
+        points: ["gameDev", "animation", "2-dAnimation"],
+        next: 1
+      },
+      no: {
+        choice: "no",
+        points: ["intDesign", "webDesign", "UX"],
+        next: 2
+      }
+    },
+    previous: null
+  },
+  {
+    number: 1,
+    question: "Do you like to create art?",
+    answers: {
+      yes: {
+        choice: "yes",
+        points: ["gameArt", "animation", "3-dAnimation"],
+        next: 3
+      },
+      no: {
+        choice: "no",
+        points: ["gameDev", "conceptArt", "2-dAnimation"],
+        next: 4
+      }
+    },
+    previous: 0 // previous method will have to subtract point that was just added to hashmap
+  }
+];
+
+/*
+ * Build the quiz
+ */
+buildQuiz();
+
+/*
+ * Event Listeners
+ */
+submitButton.addEventListener("click", showResults);
